cb791dd08b2a45e91ab24e44399a283f09da0aac
Ashiq Sulaiman <Ashiq@Ashiqs-MacBook-Pro.local>
2019-06-22 11:56:08 +0530

    initial commit

---
+++ BLESampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLESampleApp
+//
+//  Created by Ashiq Sulaiman on 22/06/19.
+//  Copyright © 2019 Ashiq Sulaiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
